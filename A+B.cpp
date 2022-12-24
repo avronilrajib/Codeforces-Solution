f16258ad868259e88d13392b1ba53b8ec32835c8
avronil rajib <110673121+avronilrajib@users.noreply.github.com>
2022-12-24 23:10:06 +0600

    Add files via upload

---
+++ A+B.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b;
+       scanf("%d + %d",&a,&b);
+       int sum=a+b;
+       cout<<sum<<endl;
+    }
+    return 0;
+}
